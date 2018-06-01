472e8b2b0c5af150184a562920c23a081da1333f
zdjjs <ishii@sencorp.co.jp>
2018-06-01 18:08:53 +0900

    ファイルをストレージに保存

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ImageRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -35,6 +36,8 @@ class ImageController extends Controller
      */
     public function store(ImageRequest $request)
     {
+    	Storage::putFile('public', $request->file);
+
         return redirect()->route('index');
     }
 
