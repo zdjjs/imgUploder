a9b7aea7dd2954d7350efe8247a8318cfaf142d0
kawata <kawata@sencorp.co.jp>
2018-06-08 15:59:30 +0900

    el-form

--- resources/assets/js/components/MainComponent.vue
+++ resources/assets/js/components/MainComponent.vue
@@ -2,17 +2,25 @@
   <div>
     <h1>画像アップローダー</h1>
     <h2>画像をアップロードする</h2>
-    <el-upload
-    ref="upload"
-    action=""
-    :limit="1"
-    :auto-upload="false">
-      <el-button slot="trigger" size="small" type="primary">
-        クリックしてファイルをアップロード
-      </el-button>
-    </el-upload><br>
-    コメント<el-input v-model="comment"/><br>
-    <el-button type="success" @click="uploadImage">アップロード</el-button>
+    <el-form>
+      <el-form-item label="アップロード">
+        <el-upload
+        ref="upload"
+        action=""
+        :limit="1"
+        :auto-upload="false">
+          <el-button slot="trigger" size="small" type="primary">
+            クリックしてファイルをアップロード
+          </el-button>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        コメント<el-input v-model="comment"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success" @click="uploadImage">アップロード</el-button>
+      </el-form-item>
+    </el-form>
     <h2>アップロード画像一覧</h2>
     <el-row type="flex" style="flex-wrap:wrap;">
       <image-component
